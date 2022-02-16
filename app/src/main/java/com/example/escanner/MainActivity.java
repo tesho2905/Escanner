b8cfd1a929ebe214c6018f703a0ca4d1cf4c6ca2
tesho2905 <cristian.telloperez@gmail.com>
2022-02-16 18:06:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/escanner/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.escanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnScan;
+    EditText txtResultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnScan = findViewById(R.id.btnScan);
+        txtResultado = findViewById(R.id.txtResultado);
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                IntentIntegrator integrador = new IntentIntegrator(MainActivity.this);
+                integrador.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrador.setPrompt("Lector - PRUEBA");
+                integrador.setCameraId(0);
+                integrador.setBeepEnabled(true);
+                integrador.setBarcodeImageEnabled(true);
+                integrador.initiateScan();
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(result != null){
+            if(result.getContents() == null){
+                Toast.makeText(this, "Lectura cancelada", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                txtResultado.setText(result.getContents());
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+}
